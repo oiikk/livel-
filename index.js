c382585d4c70cbeea33030e1a0a758011281029f
oiikk <97132856+oiikk@users.noreply.github.com>
2026-03-09 07:43:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ client.on("messageCreate", async (message) => {
     ctx.fillRect(20, 20, 760, canvasHeight - 40);
 
     // ===== صورة كبيرة على العنوان (GIF أو PNG) =====
-    const titleImageURL = "https://images-ext-1.discordapp.net/external/ZZG2yreNpi4nfuEU3l96c1qq2qcW7MXHlUxoYHufdo8/https/media.tenor.com/rSYM03_nRcEAAAAi/mi.gif?width=450&height=450"; // ضع رابط الصورة هنا
+    const titleImageURL = "https://media.tenor.com/8JhcC4OtwC8AAAAM/hatsune-miku-dance.gif"; // ضع رابط الصورة هنا
     try {
       const img = await loadImage(titleImageURL);
       ctx.drawImage(img, 20, 10, 80, 80); // x, y, width, height
@@ -194,7 +194,7 @@ client.on("messageCreate", async (message) => {
 
     ctx.font = "bold 32px Cairo";
     ctx.fillStyle = "#ffffff";
-    ctx.fillText("🏆 Top 10 Levels", 120, 50);
+    ctx.fillText(" Top 10 ", 120, 50);
 
     let yOffset = 100;
 
